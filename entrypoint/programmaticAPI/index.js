29b9dc9dde807002fb045f5419574ef7ee2b5c8e
Safi <sfmissive@gmail.com>
2019-05-08 04:37:58 +0300

    📦 Update package.json and .gitignore.

--- entrypoint/programmaticAPI/index.js
+++ entrypoint/programmaticAPI/index.js
@@ -1,7 +1,9 @@
-const projectConfig = require('../../configuration'), path = require('path'), filesystem = require('fs')
+const projectConfig = require('../../configuration'),
+  path = require('path'),
+  filesystem = require('fs')
 
 // • Run
-if(filesystem.existsSync(projectConfig.directory.distribution)) {
+if (filesystem.existsSync(projectConfig.directory.distribution)) {
   module.exports = require(projectConfig.directory.distribution)
 } else {
   // • Transpilation (babelJSCompiler)
@@ -9,8 +11,4 @@ if(filesystem.existsSync(projectConfig.directory.distribution)) {
   let compiler = new Compiler({ babelTransformConfig: projectConfig.transpilation.babelConfig })
   compiler.requireHook()
   module.exports = require(path.join(projectConfig.directory.source, projectConfig.entrypoint.programmaticAPI))
-  // way to output runtime transpilation in circular dependency.
-  // process.nextTick(() => {
-  //   console.log(compiler.loadedFiles) // write any newer files transpiled in successive usage of this module.
-  // })
 }
